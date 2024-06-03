d8fbb4ce6284df3130066c68fd7e6d6530b4b62f
Eduardo Zaduski <eduardozaduski@gmail.com>
2024-06-03 20:01:56 -0300

    Primeiro commit

---
+++ filemanager/FileManager.java
@@ -0,0 +1,23 @@
+package filemanager;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class FileManager {
+    
+    
+
+}
